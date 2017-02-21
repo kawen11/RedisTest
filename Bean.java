0444bf278f4e6e8bc1ee7afd0fbc02b21ff3ef1e
jiangzaixing <jiangzaixing@staff.hexun.com>
2017-02-21 15:48:17 +0800

    hahahaha

--- Bean.java
+++ Bean.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class Bean  implements Serializable {
 
 	 /**
-	 * 
+	 * hahahahahahahah
 	 */
 	private static final long serialVersionUID = 1L;
 	private String name;
